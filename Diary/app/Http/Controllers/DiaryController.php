9b3b8312a4a808ab7598d4a7f53b38df6b3a88fc
nexseedcamille <ykamimori@nexseed.net>
2019-12-11 15:33:54 +0800

    新規投稿機能追加

--- Diary/app/Http/Controllers/DiaryController.php
+++ Diary/app/Http/Controllers/DiaryController.php
@@ -17,4 +17,22 @@ class DiaryController extends Controller
 
         return view('diaries.index', ['diaries' => $diaries]);
     }
+
+    public function create()
+    {
+        // views/diaries/create.blade.phpを表示する
+        return view('diaries.create');
+    }
+
+    public function store(Request $request)
+    {
+        $diary = new Diary();
+        // dd('ここに保存処理');
+
+        $diary->title = $request->title;
+        $diary->body = $request->body;
+        $diary->save();
+
+        return redirect()->route('diary.index');
+    }
 }
